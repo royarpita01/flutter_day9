3aaba720c0a4d311368e07098fb899cd2a958ea6
Arpita Roy <arpita653@gmail.com>
2023-02-19 01:39:59 +0600

    name

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp
+      (home: Text('Arpita Roy'),
+    );
+  }
+}
